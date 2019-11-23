068522010f490049820dfbd1cbbf0ed5ce932187
Tobias Wiens <tobias.wiens@balluff.de>
2019-11-23 19:30:55 +0100

    basic layout and first  code, minimal setup

---
+++ script.js
@@ -0,0 +1,24 @@
+var activeItem;
+
+function setVisibleById(idToShow) {
+    document.getElementById(idToShow).style.visibility='visible';
+    document.getElementById("returnButton").style.visibility='visible';
+    
+    activeItem = idToShow;
+}
+
+function setHiddenById() {
+    document.getElementById(activeItem).style.visibility='hidden';
+    document.getElementById("returnButton").style.visibility='hidden';
+}
+
+document.getElementById("B1").addEventListener("click", function () { setVisibleById("v1") });
+document.getElementById("B2").addEventListener("click", function () { setVisibleById("v2") });
+document.getElementById("B3").addEventListener("click", function () { setVisibleById("v3") });
+document.getElementById("B4").addEventListener("click", function () { setVisibleById("v4") });
+document.getElementById("B5").addEventListener("click", function () { setVisibleById("v5") });
+document.getElementById("B6").addEventListener("click", function () { setVisibleById("v6") });
+document.getElementById("B7").addEventListener("click", function () { setVisibleById("v7") });
+document.getElementById("B8").addEventListener("click", function () { setVisibleById("v8") });
+
+document.getElementById("returnButton").addEventListener("click", function () { setHiddenById() });
