cac8fa64d5064bfabf3176ba07c577bc3033772c
Tobias Wiens <tobias.wiens@balluff.de>
2019-12-01 17:08:25 +0100

    added animations to html, background image

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 var activeItem;
 
-function setVisibleById(idToShow) {
+function displaySelectedElement(idToShow) {
+
+    hideGridButtons();
+
     document.getElementById(idToShow).style.display='block';
     document.getElementById("returnButton").style.display='block';
     
     activeItem = idToShow;
 }
 
-function setHiddenById() {
+function hideActiveElement() {
+
+    showGridButtons();
+
     document.getElementById(activeItem).style.display='none';
     document.getElementById("returnButton").style.display='none';
 }
 
-document.getElementById("B1").addEventListener("click", function () { setVisibleById("v1") });
-document.getElementById("B2").addEventListener("click", function () { setVisibleById("v2") });
-document.getElementById("B3").addEventListener("click", function () { setVisibleById("v3") });
-document.getElementById("B4").addEventListener("click", function () { setVisibleById("v4") });
-document.getElementById("B5").addEventListener("click", function () { setVisibleById("v5") });
-document.getElementById("B6").addEventListener("click", function () { setVisibleById("v6") });
+function hideGridButtons() {
+    
+    var gridButtons = document.getElementsByClassName("gridButton");
+    for (button of gridButtons) {
+        button.style.display='none';    
+    }
 
-document.getElementById("returnButton").addEventListener("click", function () { setHiddenById() });
+    var gridText = document.getElementsByClassName("gridText");
+    for (text of gridText) {
+        text.style.display='none';    
+    }
+}
+
+function showGridButtons() {
+    
+    var gridButtons = document.getElementsByClassName("gridButton");
+    for (button of gridButtons) {
+        button.style.display='block';    
+    }
+
+    var gridText = document.getElementsByClassName("gridText");
+    for (text of gridText) {
+        text.style.display='block';    
+    }
+}
+
+document.getElementById("B1").addEventListener("click", function () { displaySelectedElement("v1") });
+document.getElementById("B2").addEventListener("click", function () { displaySelectedElement("v2") });
+document.getElementById("B3").addEventListener("click", function () { displaySelectedElement("v3") });
+document.getElementById("B4").addEventListener("click", function () { displaySelectedElement("v4") });
+document.getElementById("B5").addEventListener("click", function () { displaySelectedElement("v5") });
+document.getElementById("B6").addEventListener("click", function () { displaySelectedElement("v6") });
+
+document.getElementById("returnButton").addEventListener("click", function () { hideActiveElement() });
