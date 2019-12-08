ef8a1172ce3ebecd08248bdb258acb9ec77a231c
Tobias Wiens <tobias.wiens@balluff.de>
2019-12-08 18:05:18 +0100

    added basic music player and song to the repo

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+
+//header clock element and functions
+
+//load clock when content is loaded
+window.onload = showUpdateClock;
+
+function setupClock() {
+    var refreshRateMs = 1000;
+    mytime = setTimeout("showUpdateClock()", refreshRateMs);
+}
+
+function showUpdateClock() {
+    var dateTime = new Date()
+    var seconds = dateTime.getSeconds();
+    var minutes =  dateTime.getMinutes();
+    var hours = dateTime.getHours();
+    var days = dateTime.getDate();
+    var months = dateTime.getMonth() + 1;
+
+    if(seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if(minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if(hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if(days < 10) {
+        days = "0" + days;
+    }
+
+    if(months < 10) {
+        months = "0" + months;
+    }
+
+    var timeString = hours + ":" + minutes + ":" +  seconds + "\n\n" + 
+    days + "." + months + "." + dateTime.getFullYear(); 
+
+    document.getElementById("date").innerHTML = timeString; 
+    setupClock(); //reset timer
+}
+
+
+// menu selector code, used to show and hide blocks and navigate through the web app - pages
 var activeItem;
 
 function displaySelectedElement(idToShow) {
@@ -29,6 +77,11 @@ function hideGridButtons() {
     for (text of gridText) {
         text.style.display='none';    
     }
+
+    var gridText = document.getElementsByClassName("centerGrid");
+    for (text of gridText) {
+        text.style.display='none';    
+    }
 }
 
 function showGridButtons() {
@@ -39,6 +92,11 @@ function showGridButtons() {
     }
 
     var gridText = document.getElementsByClassName("gridText");
+    for (text of gridText) {
+        text.style.display='block';    
+    }
+
+    var gridText = document.getElementsByClassName("centerGrid");
     for (text of gridText) {
         text.style.display='block';    
     }
